1d4073e1d07deb4312006ec1ee51bb95ae7ae4a5
M-Ramadan <mr.uiux.dev@gmail.com>
2026-08-03 14:48:03 +0300

    DB Mongo connection

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./db");
 
 dotenv.config({ path: ".env" });
+connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "dev") app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Our API 1");
