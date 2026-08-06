819dadb1254d7acf83cca532c99c9ef90348609c
M-Ramadan <mr.uiux.dev@gmail.com>
2026-08-06 11:36:05 +0300

    Populate category name slug

--- controllers/subCategory.controller.js
+++ controllers/subCategory.controller.js
@@ -4,6 +4,45 @@ const SubCategory = require("../models/subCategory.model");
 const APIError = require("../utils/apiError");
 const Category = require("../models/category.model");
 
+//* @desc Get list subcategories
+//* @route GET /subcategories
+//* @access Public
+const getSubCategories = asyncHandler(async (req, res, _next) => {
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 5;
+  const skip = (page - 1) * limit;
+
+  const subCategories = await SubCategory.find()
+    .skip(skip)
+    .limit(limit)
+    .populate({
+      path: "category",
+      select: "name slug -_id"
+    });
+
+  res
+    .status(200)
+    .json({ results: subCategories.length, page: page, data: subCategories });
+});
+
+//* @desc Get specific subcategory
+//* @route GET /subcategories
+//* @access Public
+const getSubCategoryById = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const subCategory = await SubCategory.findById(id).populate({
+    path: "category",
+    select: "name slug -_id"
+  });
+
+  if (!subCategory) {
+    return next(new APIError("Subcategory not found", 404));
+  }
+
+  res.status(200).json({ data: subCategory });
+});
+
 //* @desc Create subcategory
 //* @route POST /subcategories
 //* @access Private
@@ -64,36 +103,6 @@ const deleteSubcategoryById = asyncHandler(async (req, res, next) => {
   } else {
     res.status(200).json({ msg: "Subcategory deleted successfully!" });
   }
-});
-
-//* @desc Get list subcategories
-//* @route GET /subcategories
-//* @access Public
-const getSubCategories = asyncHandler(async (req, res, _next) => {
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 5;
-  const skip = (page - 1) * limit;
-
-  const subCategories = await SubCategory.find().skip(skip).limit(limit);
-
-  res
-    .status(200)
-    .json({ results: subCategories.length, page: page, data: subCategories });
-});
-
-//* @desc Get specific subcategory
-//* @route GET /subcategories
-//* @access Public
-const getSubCategoryById = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-
-  const subCategory = await SubCategory.findById(id);
-
-  if (!subCategory) {
-    return next(new APIError("Subcategory not found", 404));
-  }
-
-  res.status(200).json({ data: subCategory });
 });
 
 module.exports = {
